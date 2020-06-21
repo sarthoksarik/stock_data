ee81e7ac4328566a71b61d6cd9a98a36acf8da2f
sarthoksarik <kirasnamdas@gmail.com>
2020-06-21 13:48:51 +0600

    edited to make it take the stock name in code automatically

--- get_tickers.py
+++ get_tickers.py
@@ -14,50 +14,50 @@ groups = ["NYSE", "AMEX", "NASDAQ"]
 
 chrome_header = {"User-Agent": user_agent()}
 # print(chrome_header)
-group_name = "NYSE"
-main_url = "http://www.eoddata.com/stocklist/" + group_name + "/"
+for group in groups:
 
+    group_name = group
+    main_url = "http://www.eoddata.com/stocklist/" + group_name + "/"
 
-def scrape_main(url_chunk):
+    def scrape_main(url_chunk):
 
-    url_full = main_url + url_chunk + ".htm"
+        url_full = main_url + url_chunk + ".htm"
 
-    print("Starting " + url_full)
+        print("Starting " + url_full)
 
-    session = HTMLSession()
+        session = HTMLSession()
 
-    sr = session.get(url_full, headers=chrome_header)
-    # selector for table
-    sel_table = "#ctl00_cph1_divSymbols > table"
+        sr = session.get(url_full, headers=chrome_header)
+        # selector for table
+        sel_table = "#ctl00_cph1_divSymbols > table"
 
-    # content of the table
-    t_body = sr.html.find(sel_table)[0]
+        # content of the table
+        t_body = sr.html.find(sel_table)[0]
 
-    # all the rows as list
-    # first row is the header we are ignoring that for a while
-    #t_rows = t_body.find('tr')[1:]
+        # all the rows as list
+        # first row is the header we are ignoring that for a while
+        #t_rows = t_body.find('tr')[1:]
 
-    soup_table = bs(t_body.html, "lxml")
+        soup_table = bs(t_body.html, "lxml")
 
-    t_rows = soup_table.find_all("tr")[1:]
+        t_rows = soup_table.find_all("tr")[1:]
 
-    with open(group_name + "_all_ticker_list.csv", "a", newline="") as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=",")
+        with open(group_name + "_all_ticker_list.csv", "a", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=",")
 
-        for row in t_rows:
-            csv_row = []
-            tds = row.find_all("td")
-            data1 = tds[0].a.text
-            csv_row.append(data1)
-            data2_6 = [td.text for td in tds[1:6]]
-            csv_row.extend(data2_6)
-            csv_writer.writerow(csv_row)
+            for row in t_rows:
+                csv_row = []
+                tds = row.find_all("td")
+                data1 = tds[0].a.text
+                csv_row.append(data1)
+                data2_6 = [td.text for td in tds[1:6]]
+                csv_row.extend(data2_6)
+                csv_writer.writerow(csv_row)
 
-    print("Completed: " + url_full)
+        print("Completed: " + url_full)
 
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(scrape_main, letters)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(scrape_main, letters)
 
 # scrape_main()
 
